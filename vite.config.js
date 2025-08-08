a13ef6e1bbcf50ab62861f5380228490cd3a28d3
kevin344k <kevin344k@gmail.com>
2025-08-08 18:29:51 -0500

    sdfsdf

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:"https://kevin344k@github.io/jj-react-web",
   optimizeDeps: {
     include: ['@leenguyen/react-flip-clock-countdown'],
   },
